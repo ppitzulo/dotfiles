d29fa9798c4e9b89df6dc0e6f1672c1254dd4830
Peter Pitzulo <plpitzulo@protonmail.com>
2024-05-15 11:58:09 -0400

    Fix nvim configs not being added due to child git repo

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,13 @@
+local general_on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "tailwindcss", "emmet_ls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = general_on_attach,
+    capabilities = capabilities,
+  }
+end
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,168 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+      -- injector = { ---@type table<lc.lang, lc.inject>
+      --   ["python3"] = {
+      --     before = true
+      --   },
+      --   ["cpp"] = {
+      --     before = { "#include <bits/stdc++.h>", "using namespace std;" },
+      --     after = "int main() {}",
+      --   },
+      --   ["java"] = {
+      --     before = "import java.util.*;",
+      --   },
+      -- }
+    },
+    lazy = false,
+  },
+  {
+    'easymotion/vim-easymotion',
+    lazy = false,
+    config = function()
+      vim.api.nvim_set_keymap('n', '<Leader>s', '<Plug>(easymotion-s2)', { noremap = true })
+    end
+  },
+  {
+    'mg979/vim-visual-multi',
+    lazy = false,
+    config = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-m>",
+        ["Find Subword Under"] = "<C-m>",
+      }
+      vim.api.nvim_set_keymap('n', '<C-m>', '<Plug>(VM-Find-Under)', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('x', '<C-m>', '<Plug>(VM-Find-Subword-Under)', { noremap = true, silent = true })
+    end
+  },
+  {
+    "themaxmarchuk/tailwindcss-colors.nvim",
+    lazy = true,
+    config = function ()
+      require("tailwindcss-colors").setup()
+    end
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- opts = overrides.treesitter,
+    opts = {
+      ensure_installed = {
+        "vim",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "cpp"
+      }
+    }
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    lazy = false,
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-g>"
+      }
+    end,
+  },
+  {
+    "dharmx/telescope-media.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          media = {
+            backend = "ueberzug", -- image/gif backend
+            backend_options = {
+              viu = {
+                move = true, -- GIF preview
+              },
+            },
+            -- on_confirm_single = canned.single.copy_path,
+            -- on_confirm_muliple = canned.multiple.bulk_copy,
+            cache_path = vim.fn.stdpath("cache") .. "/media",
+          },
+        },
+      })
+      require("telescope").load_extension("media")
+    end,
+  }
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
+}
+
+return plugins
